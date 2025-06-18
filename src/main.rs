624644e3d73dd0134c3f06ef23183776ac7abc52
Alan Evans <thealanevans@gmail.com>
2025-06-18 02:06:38 -0400

    Chapter 1 Tuples, Points, Vectors - simple demo of some vector and point math

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use crate::tuple::Tuple;
 mod tuple;
 
 fn main() {
-    let vector = Tuple::vector(1.0, 2.0, 3.0);
-    println!("Hello, world! {vector}");
+    let vector = Tuple::point(1.0, 2.0, 3.0);
+    let speed = Tuple::vector(0.1, 0.2, -0.1);
+    for i in 0..100 {
+        println!("{}", vector + speed * i as f32)
+    }
 }
