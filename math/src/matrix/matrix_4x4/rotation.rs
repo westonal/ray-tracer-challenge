afb0404e576794e23a47a360cb2796fff3f788ff
Alan Evans <thealanevans@gmail.com>
2025-06-21 16:12:04 -0400

    Chapter 4: 4x4 Matrix transforms - rotate z

--- math/src/matrix/matrix_4x4/rotation.rs
+++ math/src/matrix/matrix_4x4/rotation.rs
@@ -24,6 +24,18 @@ impl Matrix4x4 {
         m[3][3] = 1.;
         m
     }
+
+    pub fn rotation_z(z_radians: f32) -> Matrix4x4 {
+        let mut m = Self::empty();
+        let (sin, cos) = z_radians.sin_cos();
+        m[0][0] = cos;
+        m[0][1] = -sin;
+        m[1][0] = sin;
+        m[1][1] = cos;
+        m[2][2] = 1.;
+        m[3][3] = 1.;
+        m
+    }
 }
 
 #[cfg(test)]
@@ -142,6 +154,60 @@ mod rotation_y_tests {
         );
         assert_eq!(
             Tuple::new(-1.4142137, 2., 2.8284273, 1.),
+            eighth_inverted * p
+        );
+    }
+}
+
+#[cfg(test)]
+mod rotation_z_tests {
+    use crate::matrix::matrix_4x4::Matrix4x4;
+    use crate::tuple::Tuple;
+    use crate::tuple::point::Point;
+
+    use std::f32::consts::PI;
+
+    #[test]
+    fn rotate_point_one_eighth() {
+        let eighth = Matrix4x4::rotation_z(PI / 4.0);
+        let p = Point::point(0., 1., 0.);
+        assert_eq!(
+            eighth * p,
+            Point::point(
+                -std::f32::consts::FRAC_1_SQRT_2,
+                std::f32::consts::FRAC_1_SQRT_2,
+                0.,
+            )
+            .into()
+        );
+    }
+
+    #[test]
+    fn rotate_point_one_eighth_2() {
+        let eighth = Matrix4x4::rotation_z(PI / 4.0);
+        let p = Point::point(2., 3., 2.);
+        assert_eq!(eighth * p, Point::point(-0.7071067, 3.535534, 2.,).into());
+    }
+
+    #[test]
+    fn rotate_point_one_quarter() {
+        let quarter = Matrix4x4::rotation_z(PI / 2.0);
+        let p = Point::point(0., 1., 0.);
+        assert_eq!(quarter * p, Point::point(-1., -4.371139e-8, 0.).into());
+    }
+
+    #[test]
+    fn rotate_point_one_eighth_inverted() {
+        let eighth = Matrix4x4::rotation_z(PI / 4.0);
+        let eighth_reversed = Matrix4x4::rotation_z(-(PI / 4.0));
+        let eighth_inverted = eighth.invert().unwrap();
+        let p = Point::point(1., 2., 3.);
+        assert_eq!(
+            Tuple::new(2.1213202, 0.70710677, 3., 1.),
+            eighth_reversed * p
+        );
+        assert_eq!(
+            Tuple::new(2.1213205, 0.7071068, 3., 1.),
             eighth_inverted * p
         );
     }
