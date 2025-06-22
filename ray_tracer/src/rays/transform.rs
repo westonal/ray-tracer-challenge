a4b81d681efd4d2265ebc24eee3f5798187e7bab
Alan Evans <thealanevans@gmail.com>
2025-06-21 23:08:40 -0400

    Chapter 5: Ray-Sphere Intersections - ray transforms as Mul

--- ray_tracer/src/rays/transform.rs
+++ ray_tracer/src/rays/transform.rs
@@ -1,15 +1,18 @@
 use crate::rays::Ray;
 use math::matrix::matrix_4x4::Matrix4x4;
+use std::ops::Mul;
 
-impl Ray {
-    pub fn transform(&self, m: Matrix4x4) -> Self {
-        let point = (m * self.origin)
+impl Mul<Ray> for Matrix4x4 {
+    type Output = Ray;
+
+    fn mul(self, rhs: Ray) -> Self::Output {
+        let point = (self * rhs.origin)
             .try_into()
             .expect("Matrix supplied mapped point to non-point");
-        let direction = (m * self.direction)
+        let direction = (self * rhs.direction)
             .try_into()
             .expect("Matrix supplied mapped point to non-vector");
-        Self::new(point, direction)
+        Ray::new(point, direction)
     }
 }
 
@@ -22,7 +25,7 @@ mod ray_transform_tests {
     fn translate_a_point_in_a_ray() {
         let ray = ray!((1., 2., 3.), (0., 1., 0.));
         assert_eq!(
-            ray.transform(Matrix4x4::translation(3., 4., 5.)),
+            Matrix4x4::translation(3., 4., 5.) * ray,
             ray!((4., 6., 8.), (0., 1., 0.))
         )
     }
@@ -31,7 +34,7 @@ mod ray_transform_tests {
     fn scale_a_ray() {
         let ray = ray!((1., 2., 3.), (0., 1., 0.));
         assert_eq!(
-            ray.transform(Matrix4x4::scale(2., 3., 4.)),
+            Matrix4x4::scale(2., 3., 4.) * ray,
             ray!((2., 6., 12.), (0., 3., 0.))
         )
     }
