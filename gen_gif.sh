083a0295c1b980c7265cd4e764d7970dc5883edb
Alan Evans <thealanevans@gmail.com>
2025-08-02 18:33:48 -0400

    Published

--- gen_gif.sh
+++ gen_gif.sh
@@ -3,7 +3,7 @@
 # See https://blog.pkh.me/p/21-high-quality-gif-with-ffmpeg.html
 
 palette="/tmp/palette.png"
-filters="fps=25,scale=540:-1:flags=lanczos"
+filters="fps=25,scale=360:-1:flags=lanczos"
 
 input="test_scenes/chess_queen_material_animation.mp4"
 output="test_scenes/chess_queen_material_animation_palette.gif"
@@ -16,7 +16,7 @@ output="test_scenes/chess_queen_material_animation_palette.gif"
 
 # name="utah_teapot_animated"
 # name="chess_queen_material_animation"
-name="satisfying-pipes-raising"
+name="satisfying-conveyor"
 input="test_scenes/${name}_frames/${name}_%04d.png"
 output="test_scenes/${name}.gif"
 
